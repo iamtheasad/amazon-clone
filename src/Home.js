009a27dec85efb715d9dd8227b0924ec75162be3
iamtheasad <iamtheasad@gmail.com>
2020-09-14 17:40:10 +0600

    Banner added

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,11 @@
 import React, {Fragment} from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
         <Fragment>
             <div className="home">
-                <h1>I am homepage</h1>
+                <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg" alt="Banner Image"/>
             </div>
         </Fragment>
     );
