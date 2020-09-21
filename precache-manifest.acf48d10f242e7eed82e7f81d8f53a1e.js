0131f450685d9befac748585f056ba48dc7348ad
iamtheasad <iamtheasad@gmail.com>
2020-09-21 15:26:06 +0600

    Updates

---
+++ precache-manifest.acf48d10f242e7eed82e7f81d8f53a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8e3cfe54dca5bbd39a907cbc9b710c",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "b6f545c1c46cb0eabb37",
+    "url": "/amazon-clone/static/css/main.a4d93f04.chunk.css"
+  },
+  {
+    "revision": "263365d9471ada2d61d8",
+    "url": "/amazon-clone/static/js/2.7eccaf9f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazon-clone/static/js/2.7eccaf9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f545c1c46cb0eabb37",
+    "url": "/amazon-clone/static/js/main.6a250c27.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
