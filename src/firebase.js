1c5173a470533bc51783a973e67af90bb16d4662
iamtheasad <iamtheasad@gmail.com>
2020-09-22 09:35:58 +0600

    firebase.js added firebase & firebase-tools installed

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyD1MiGfS2vEwidNrgzG8C1YAqF5RAtF56E",
+    authDomain: "clone-3e9c3.firebaseapp.com",
+    databaseURL: "https://clone-3e9c3.firebaseio.com",
+    projectId: "clone-3e9c3",
+    storageBucket: "clone-3e9c3.appspot.com",
+    messagingSenderId: "782645067839",
+    appId: "1:782645067839:web:ed0bc4a3648fc31008788a",
+    measurementId: "G-2PSS1CHWEP",
+});
+
+const auth = firebase.auth();
+
+export { auth };
+
